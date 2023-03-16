74dc3c7917f98c2ab440e6373bf02148dcd06faa
ryansuranjana <ryansuranjana23@gmail.com>
2023-03-16 17:12:21 +0800

    fix datatable component

--- resources/js/Components/DataTable.jsx
+++ resources/js/Components/DataTable.jsx
@@ -3,8 +3,8 @@ import generatePagination from '@/Utils/generatePagination'
 import Table from 'react-bootstrap/Table';
 import { Pagination } from 'react-bootstrap';
 
-const DataTable = ({ columns, data }) => {
-    const [dataTables, setDataTable] = useState([]);
+const DataTable = ({ columns, row, data }) => {
+    const [dataTables, setDataTables] = useState([]);
     const [pagePagination, setPagePagination] = useState(0);
     const [pageCurrent, setCurrentPage] = useState(0);
 
@@ -17,38 +17,44 @@ const DataTable = ({ columns, data }) => {
 
     useEffect(() => {
         const pagination = generatePagination(data);
-        setDataTable(generatePagination(pagination.data))
+        setDataTables(pagination.data)
         setPagePagination(pagination.totalPage);
-    }, data)
+    }, [data])
     return (
         <>
-            <Table striped bordered hover>
+            <Table hover responsive>
                 <thead>
-                    <tr>
-                        {columns.map(column => (
-                            <td>{column.name}</td>
+                    <tr className='fw-bold'>
+                        {columns.map((column, i) => (
+                            <td key={i}>{column}</td>
                         ))}
                     </tr>
                 </thead>
                 <tbody>
                     {dataTables.length > 0 ? (
-                        dataTables[pageCurrent].map((dataTable) => {
-                            return (<tr>
-                                {dataTable.values().map((value) => (
-                                        <td>{value}</td>
-                                ))}
-                            </tr>)
+                        dataTables[pageCurrent].map((dataTable, j) => {
+                            return (
+                                <tr key={j}>
+                                    {row.map((element, i) => {
+                                        if(dataTable.hasOwnProperty(element)) {
+                                            return (
+                                                <td key={i}>{dataTable[element]}</td>
+                                            )
+                                        }
+                                    })}
+                                </tr>
+                            )
                         })
                     ) : (
                         <tr>
-                            <td colSpan={columns.length + 1}>Data tidak ditemukan!</td>
+                            <td colSpan={columns.length + 1} className="text-center">Data tidak ditemukan!</td>
                         </tr>
                     )}
                 </tbody>
             </Table>
             <Pagination>
                 <Pagination.Item disabled={pageCurrent === 0} onClick={handlePagePrevious}>Previous</Pagination.Item>
-                <Pagination.Item disabled={pageCurrent === pagePagination} onClick={handlePageNext}>Next</Pagination.Item>
+                <Pagination.Item disabled={pagePagination === pageCurrent + 1} onClick={handlePageNext}>Next</Pagination.Item>
             </Pagination>
         </>
     )
