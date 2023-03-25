8ee5a62801d80e59403ce9b334d72bad77333294
ryansuranjana <ryansuranjana23@gmail.com>
2023-03-25 13:43:36 +0800

    fix sidebar

--- resources/js/Partials/Sidebar.jsx
+++ resources/js/Partials/Sidebar.jsx
@@ -4,26 +4,14 @@ import React from "react";
 const Sidebar = ({ auth }) => {
     return (
         <aside className="main-sidebar sidebar-dark-primary elevation-4">
-            <a href={route('home')} className="brand-link">
-                <img
-                    src="dist/img/AdminLTELogo.png"
-                    alt="AdminLTE Logo"
-                    className="brand-image img-circle elevation-3"
-                    style={{ opacity: ".8" }}
-                />
+            <a href={route('home')} className="brand-link text-center">
                 <span className="brand-text font-weight-light">Ryu Gate</span>
             </a>
             <div className="sidebar">
                 <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div className="image">
-                        <img
-                            src="dist/img/user2-160x160.jpg"
-                            className="img-circle elevation-2"
-                            alt="User Image"
-                        />
-                    </div>
                     <div className="info">
                         <a href="#" className="d-block">
+                            <i className="fas fa-user-circle text-white mr-2" style={{ fontSize: '1.5rem' }}></i>
                             {auth.user.name}
                         </a>
                     </div>
@@ -36,20 +24,20 @@ const Sidebar = ({ auth }) => {
                         data-accordion="false"
                     >
                         <li className="nav-item mb-1">
-                            <Link href={route('dashboard')} className="nav-link">
+                            <Link href={route('dashboard')} className={`nav-link ${route().current('dashboard') ? 'active' : ''}`}>
                                 <i className="nav-icon fas fa-tachometer-alt" />
                                 <p>Dashboard</p>
                             </Link>
                         </li>
                         <li className="nav-header">MASTER DATA</li>
                         <li className="nav-item mb-1">
-                            <Link href={route('classrooms.index')} className="nav-link">
+                            <Link href={route('classrooms.index')} className={`nav-link ${route().current('classrooms.*') ? 'active' : ''}`}>
                                 <i className="nav-icon fas fa-table" />
                                 <p>Kelas</p>
                             </Link>
                         </li>
                         <li className="nav-item mb-1">
-                            <Link href={route('students.index')} className="nav-link">
+                            <Link href={route('students.index')} className={`nav-link ${route().current('students.*') ? 'active' : ''}`}>
                                 <i className="nav-icon fas fa-graduation-cap" />
                                 <p>Siswa</p>
                             </Link>
